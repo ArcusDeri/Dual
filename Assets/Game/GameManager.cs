d62dd4d3cfc443337b2dcf6bdd508da6a566f91a
michniak@yandex.com <michniak@yandex.com>
2018-04-02 20:11:47 +0200

    Set "S" button as Pause button. Create pausing option by changing the time scale to 0.

--- Assets/Game/GameManager.cs
+++ Assets/Game/GameManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
@@ -6,7 +7,13 @@ using UnityStandardAssets.CrossPlatformInput;
 public class GameManager : MonoBehaviour {
 
 	public bool IsRecording = true;
+
+	private bool IsPaused = false;
+	private float m_FixedDeltaTime;
 	
+	void Start(){
+		m_FixedDeltaTime = Time.fixedDeltaTime;
+	}
 	// Update is called once per frame
 	void Update () {
 		if(CrossPlatformInputManager.GetButton("Fire1")){
@@ -14,5 +21,21 @@ public class GameManager : MonoBehaviour {
 		}else{
 			IsRecording = true;
 		}
+		if(CrossPlatformInputManager.GetButtonDown("Pause")){
+			SwitchPauseState();
+		}
 	}
+
+    private void SwitchPauseState()
+    {
+		if(!IsPaused){
+        	Time.timeScale = 0f;
+			Time.fixedDeltaTime = 0f;
+		}
+		else{
+			Time.timeScale = 1f;
+			Time.fixedDeltaTime = m_FixedDeltaTime;
+		}
+		IsPaused = !IsPaused;
+    }
 }
