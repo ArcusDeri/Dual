a067113fcf598be6b5ee3cd356df869802638c81
Marcin Michniak <michniak@yandex.com>
2018-04-14 21:51:33 +0200

    Prefab the finish box. Scale it to make it wider. Add script that moves the player to the origin and sets win message when reached finish box. I consider this project done.

---
+++ Assets/FinishBox/FinishBox.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishBox : MonoBehaviour {
+
+	private Player m_Player;
+	private MessageBox m_MessageBox;
+
+	void Start(){
+		m_Player = GameObject.FindObjectOfType<Player>();
+		m_MessageBox = GameObject.FindObjectOfType<MessageBox>();
+	}
+	void OnTriggerEnter(Collider coll){
+		m_Player.MoveToOrigin();
+		m_MessageBox.SetMessage("Well done! You won!",10);
+	}
+}
